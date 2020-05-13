30addceec63f0319a1967e1d50480fd8a835d425
Thomas Gonda <passinto06@gmail.com>
2020-05-13 11:31:10 -0700

    thomas commits

--- src/App.js
+++ src/App.js
@@ -7,24 +7,51 @@ class App extends Component{
     super(props)
     this.state = {
       squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
-      treasure: ""
+      treasure: "",
+      bomb: "",
+      counter: 5
     }
   }
 
   questionToTree = (click) =>{
     this.setState({ squares: this.state.squares.map((value,index)=> {
-      if (index==click){
+      if (click === this.state.treasure && index === click) {
+        return "treasure"
+      } else if (click === this.state.bomb && index === click){
+        return "bomb"
+      } else if (index === click){
         return "tree"
       } else {
         return value
       }})})
+      if (this.state.treasure === click) {
+        alert("You Win")
+      }
+      if (this.state.bomb === click)  {
+        alert("You Lose")
+      }
+    this.setState({ counter: this.state.counter - 1})
+    if (this.state.counter === 1 && click != this.state.treasure && click != this.state.bomb) {
+      alert("You Lose")
+    }
   }
 
   componentDidMount = () => {
-    let randomNum = Math.floor(Math.random () * 9)
-    console.log (randomNum)
-    this.setState({treasure: randomNum})
+    let randomNum = Math.floor(Math.random () * 9);
+    let secondNum = randomNum;
+    while (randomNum === secondNum) {
+      secondNum = Math.floor(Math.random () * 9);
+    }
+    this.setState({treasure: randomNum, bomb: secondNum})
+  }
 
+  restart = () => {
+    let randomNum = Math.floor(Math.random () * 9);
+    let secondNum = randomNum;
+    while (randomNum === secondNum) {
+      secondNum = Math.floor(Math.random () * 9);
+    }
+    this.setState({ squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"], treasure: randomNum, bomb: secondNum, counter: 5 })
   }
 
   render(){
@@ -42,8 +69,9 @@ class App extends Component{
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
         <div>
+          <button onClick = { this.restart }> Restart </button>
+          { this.state.counter }
           { square }
-          { this.state.treasure }
         </div>
       </React.Fragment>
     )
