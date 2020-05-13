5828fd197312e0e565e00426251c313566c6b7ed
Chantelle Isaacs <chantelle616@gmail.com>
2020-05-13 10:58:25 -0700

    trees and random treasure done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      treasure: ""
     }
   }
 
+  questionToTree = (click) =>{
+    this.setState({ squares: this.state.squares.map((value,index)=> {
+      if (index==click){
+        return "tree"
+      } else {
+        return value
+      }})})
+  }
+
+  componentDidMount = () => {
+    let randomNum = Math.floor(Math.random () * 9)
+    console.log (randomNum)
+    this.setState({treasure: randomNum})
+
+  }
+
   render(){
+    let square = this.state.squares.map((value, index)=> {
+      return (
+        <Square
+        value = { value }
+        index = { index }
+        questionToTree = {this.questionToTree}
+        />
+      )
+    })
+
     return(
       <React.Fragment>
         <h1>Treasure Hunt App</h1>
+        <div>
+          { square }
+          { this.state.treasure }
+        </div>
       </React.Fragment>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
 class Square extends Component{
+  
+  // tellMeTheIndex = () => { 
+  //   return alert(this.props.index)
+  // }
+
+
+
   render(){
     return(
       <React.Fragment>
+        <div id = 'square'
+          onClick = {() => this.props.questionToTree(this.props.index)}>
+          {this.props.value}
+          {/* index
+          function that alerts the index we passed */}
+        </div>
       </React.Fragment>
     )
   }
